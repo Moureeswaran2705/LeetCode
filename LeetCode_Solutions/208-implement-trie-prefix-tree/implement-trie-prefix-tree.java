050064cfe3f05007eb09ff5c22af9e26bebebd48
Moureeswaran <122042961+Moureeswaran2705@users.noreply.github.com>
2024-03-14 10:10:33 +0530

    Time: 38 ms (54.28%) | Memory: 55 MB (89.20%) - LeetSync

---
+++ LeetCode_Solutions/208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
@@ -0,0 +1,52 @@
+class Trie
+{
+    class Node
+    {
+        boolean eow=false;
+        Node []children=new Node[26];
+    }
+    Node root;
+    public Trie()
+    {
+        root=new Node();
+    }
+    public void insert(String word)
+    {
+        Node node=root;
+        for(char ch:word.toCharArray())
+        {
+            if(node.children[ch-'a']==null)
+            {
+                node.children[ch-'a']=new Node();
+            }
+            node=node.children[ch-'a'];
+        }
+        node.eow=true;
+    }
+    public boolean search(String word)
+    {
+        Node node=root;
+        for(char ch:word.toCharArray())
+        {
+            if(node.children[ch-'a']==null)
+            {
+                return false;
+            }
+            node=node.children[ch-'a'];
+        }
+        return node.eow;
+    }
+    public boolean startsWith(String prefix)
+    {
+        Node node=root;
+        for(char ch:prefix.toCharArray())
+        {
+            if(node.children[ch-'a']==null)
+            {
+                return false;
+            }
+            node=node.children[ch-'a'];
+        }
+        return true;
+    }
+}
