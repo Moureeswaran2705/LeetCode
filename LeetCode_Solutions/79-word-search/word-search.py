9c7bb0de43ed86021fed78828ce94e1fc07eaf76
Moureeswaran <122042961+Moureeswaran2705@users.noreply.github.com>
2024-04-03 20:43:20 +0530

    Time: 3530 ms (76.48%) | Memory: 16.5 MB (71.59%) - LeetSync

---
+++ LeetCode_Solutions/79-word-search/word-search.py
@@ -0,0 +1,21 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        n=len(board)
+        m=len(board[0])
+        for i in range(n):
+            for j in range(m):
+                if self.dfs(board,word,i,j,0):
+                    return True
+        return False
+    def dfs(self,board:List[List[str]],word:str,i:int,j:int,s:int)->bool:
+        if i<0 or i==len(board) or j<0 or j==len(board[0]):
+            return False
+        if board[i][j]!=word[s] or board[i][j]=='*':
+            return False
+        if s==len(word)-1:
+            return True
+        cache=board[i][j]
+        board[i][j]='*'
+        exist=(self.dfs(board,word,i+1,j,s+1) or self.dfs(board,word,i-1,j,s+1) or self.dfs(board,word,i,j+1,s+1) or self.dfs(board,word,i,j-1,s+1))
+        board[i][j]=cache
+        return exist
