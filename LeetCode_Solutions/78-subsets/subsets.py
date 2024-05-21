110edab32930d15d2d264ed75f173bcffe9fd962
Moureeswaran <122042961+Moureeswaran2705@users.noreply.github.com>
2024-05-21 09:43:43 +0530

    Time: 21 ms (99.77%) | Memory: 16.7 MB (77.55%) - LeetSync

---
+++ LeetCode_Solutions/78-subsets/subsets.py
@@ -0,0 +1,14 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res=[]
+        sub=[]
+        def back(i):
+            if i==len(nums):
+                res.append(sub.copy())
+                return
+            sub.append(nums[i])
+            back(i+1)
+            sub.pop()
+            back(i+1)
+        back(0)
+        return res
