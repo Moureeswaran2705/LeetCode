81be341c6b4870c63406cd665a6a84ddc3479838
Moureeswaran <122042961+Moureeswaran2705@users.noreply.github.com>
2024-05-22 15:13:37 +0530

    Time: 477 ms (34.81%) | Memory: 35.2 MB (66.68%) - LeetSync

--- LeetCode_Solutions/131-palindrome-partitioning/palindrome-partitioning.py
+++ LeetCode_Solutions/131-palindrome-partitioning/palindrome-partitioning.py
@@ -1,21 +1,25 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        res=[]
-        path=[]
-        self.partitionHelper(0,s,res,path)
+        res = []
+        path = []
+
+        def partitionHelper(index: int):
+            if index == len(s):
+                res.append(path[:])
+                return
+            for i in range(index, len(s)):
+                if isPalindrome(s, index, i):
+                    path.append(s[index:i + 1])
+                    partitionHelper(i + 1)
+                    path.pop()
+
+        def isPalindrome(s: str, start: int, end: int) -> bool:
+            while start <= end:
+                if s[start] != s[end]:
+                    return False
+                start += 1
+                end -= 1
+            return True
+
+        partitionHelper(0)
         return res
-    def partitionHelper(self,index,s,res,path):
-        if index==len(s):
-            res.append(path[:])
-        for i in range(index,len(s)):
-            if self.isPalindrome(s,index,i):
-                path.append(s[index:i+1])
-                self.partitionHelper(i+1,s,res,path)
-                path.pop()
-    def isPalindrome(self,s,start,end):
-        while start<=end:
-            if s[start]!=s[end]:
-                return False
-            start+=1
-            end-=1
-        return True
