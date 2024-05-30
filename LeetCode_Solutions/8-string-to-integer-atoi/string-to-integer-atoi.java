62b4f007687ba39654c55e14b88a8081e9d1099f
Moureeswaran <122042961+Moureeswaran2705@users.noreply.github.com>
2024-05-30 16:21:06 +0530

    Time: 1 ms (100.00%) | Memory: 42.3 MB (58.31%) - LeetSync

--- LeetCode_Solutions/8-string-to-integer-atoi/string-to-integer-atoi.java
+++ LeetCode_Solutions/8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,24 +1,22 @@
 class Solution {
     public int myAtoi(String s) {
-        s = s.trim();
-        if (s.isEmpty()) {
+        s=s.trim();
+        if(s.isEmpty()){
             return 0;
         }
-        int pos = 0;
-        long result = 0; // Change int to long to handle overflow
-        int sign = 1;
-        if (s.charAt(0) == '-' || s.charAt(0) == '+') {
-            sign = (s.charAt(pos++) == '-') ? -1 : 1;
+        int pos=0;
+        long result=0;
+        int sign=1;
+        if(s.charAt(0)=='-'||s.charAt(0)=='+'){
+            sign=(s.charAt(pos++)=='-')?-1:1;
         }
-        while (pos < s.length() && Character.isDigit(s.charAt(pos))) {
-            int cur = s.charAt(pos++) - '0';
-            result = result * 10 + cur;
-            if (sign == 1 && result > Integer.MAX_VALUE) {
-                return Integer.MAX_VALUE;
-            } else if (sign == -1 && -result < Integer.MIN_VALUE) {
-                return Integer.MIN_VALUE;
+        while(pos < s.length() && Character.isDigit(s.charAt(pos))){
+            int cur=s.charAt(pos++)-'0';
+            result=result*10+cur;
+            if(result<Integer.MIN_VALUE||result>Integer.MAX_VALUE){
+                return (sign==1)?Integer.MAX_VALUE:Integer.MIN_VALUE;
             }
         }
-        return (int) (sign * result);
+        return (int)(sign*result);
     }
 }
