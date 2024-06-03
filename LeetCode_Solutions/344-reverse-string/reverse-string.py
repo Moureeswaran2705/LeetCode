2ef1996d379b1ef9faeac7ab959fa6f43307cad1
Moureeswaran <122042961+Moureeswaran2705@users.noreply.github.com>
2024-06-03 09:24:33 +0530

    Time: 155 ms (98.14%) | Memory: 20.8 MB (91.80%) - LeetSync

--- LeetCode_Solutions/344-reverse-string/reverse-string.py
+++ LeetCode_Solutions/344-reverse-string/reverse-string.py
@@ -1,9 +1,4 @@
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        n=len(s)
-        res=[]
-        for i in range(n-1,-1,-1):
-            s.append(s.pop(i))
-            #print(s)
-        return s
+        return s.reverse()
         
