d63bd7b00904633d765ca95b9a4872c8316ac90a
Moureeswaran <122042961+Moureeswaran2705@users.noreply.github.com>
2024-07-10 09:32:25 +0530

    Time: 1 ms (99.42%) | Memory: 42.1 MB (37.73%) - LeetSync

---
+++ LeetCode_Solutions/1720-crawler-log-folder/crawler-log-folder.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minOperations(String[] logs) {
+        int ans=0;
+        for(String log:logs){
+            if(log.equals("./")){
+                continue;
+            }
+            else if(log.equals("../")){
+                ans=Math.max(0,ans-1);
+            }
+            else{
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
