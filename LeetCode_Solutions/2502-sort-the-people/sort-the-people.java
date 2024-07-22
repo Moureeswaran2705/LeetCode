899d15c81de135640c10858d7e03c0ccc012970a
Moureeswaran <122042961+Moureeswaran2705@users.noreply.github.com>
2024-07-22 21:01:11 +0530

    Time: 9 ms (53.75%) | Memory: 45.5 MB (18.45%) - LeetSync

--- LeetCode_Solutions/2502-sort-the-people/sort-the-people.java
+++ LeetCode_Solutions/2502-sort-the-people/sort-the-people.java
@@ -1,14 +1,16 @@
 class Solution {
     public String[] sortPeople(String[] names, int[] heights) {
-        String[] sort=new String[names.length];
-        List<Pair<String,Integer>> mp= new ArrayList<>();
+        String[] sor=new String[names.length];
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        Map<Integer,String> mp=new HashMap<>();
         for(int i=0;i<names.length;i++){
-            mp.add(new Pair<>(names[i],heights[i]));
+            mp.put(heights[i],names[i]);
+            pq.add(heights[i]);
         }
-        Collections.sort(mp,(l1,l2)->l2.getValue().compareTo(l1.getValue()));
-        for(int i=0;i<sort.length;i++){
-            sort[i]=mp.get(i).getKey();
+        int j=names.length-1;
+        for(int i=names.length-1;i>=0;i--){
+            sor[i]=mp.get(pq.poll());
         }
-        return sort;
+        return sor;
     }
 }
