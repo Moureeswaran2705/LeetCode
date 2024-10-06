4b9dce63828a6666450f8d932e2c1f934eb58ea2
Moureeswaran <122042961+Moureeswaran2705@users.noreply.github.com>
2024-10-06 14:25:18 +0530

    Time: 0 ms (100.00%) | Memory: 58.1 MB (85.11%) - LeetSync

--- LeetCode_Solutions/2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.java
+++ LeetCode_Solutions/2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.java
@@ -1,21 +1,11 @@
 class Solution {
     public int chalkReplacer(int[] chalk, int k) {
-        int i=0;
-        while(k>=0){
-            if(k>=chalk[i]){
-                k-=chalk[i];
-                i++;
-            }
-            else{
-                return i;
-            }
-            if(i==chalk.length){
-                i=0;
-            }
-            
-           // System.out.print(k+" ");
-           // System.out.print(i+" ");
+        int sum = 0;
+        int n = chalk.length;
+        for(int i = 0; i < n; i++) {
+            sum += chalk[i];
+            if(sum > k) return i;
         }
-        return k;
+        return chalkReplacer(chalk, k % sum);
     }
 }
