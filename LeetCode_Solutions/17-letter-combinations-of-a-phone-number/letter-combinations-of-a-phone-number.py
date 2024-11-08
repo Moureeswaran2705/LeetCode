fb765496d25f850461d0048fe1bdbab209927b79
Moureeswaran <122042961+Moureeswaran2705@users.noreply.github.com>
2024-11-08 09:48:13 +0530

    Time: 0 ms (100.00%) | Memory: 16.7 MB (30.18%) - LeetSync

--- LeetCode_Solutions/17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ LeetCode_Solutions/17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,19 +1,18 @@
 class Solution:
-    def letterCombinations1(self, digits: str, idx: int, codes: List[str], ans: List[str], psf: str):
-        if idx == len(digits):
+    def letterCombinations1(self, digits: str, idx: int, codes: List[str], ans: List[str], psf: str):      
+        if idx==len(digits):
             ans.append(psf)
-            return 1
-        count = 0  # Corrected variable name
-        val = int(digits[idx])
-        code = codes[val]
-        for i in range(len(code)):
-            count += self.letterCombinations1(digits, idx + 1, codes, ans, psf + code[i])
-        return count
+            return
+        val=int(digits[idx])
+        s=codes[val]
+        for i in range(len(s)):
+            self.letterCombinations1(digits,idx+1,codes,ans,psf+s[i])
+        
     
     def letterCombinations(self, digits: str) -> List[str]:
         if not digits:
             return []
-        codes = ['', '', 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz']
-        ans = []
-        self.letterCombinations1(digits, 0, codes, ans, "")
+        codes=["","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"]
+        ans=[]
+        self.letterCombinations1(digits,0,codes,ans,"")
         return ans
